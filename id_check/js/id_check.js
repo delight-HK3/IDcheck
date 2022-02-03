fd4639b4b98048cf8feb48bf95b957c591d98d1a
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-03 13:35:28 +0900

    Update id_check.js

--- id_check/js/id_check.js
+++ id_check/js/id_check.js
@@ -3,7 +3,7 @@ function check_id() {
 	let success = "&nbsp&nbsp&nbsp<font style='color:rgb(0, 201, 0)'>사용가능한 아이디입니다.</font>";
 	let error = "&nbsp&nbsp&nbsp<font style='color:red'>아이디가 중복됩니다.</font>";
 	$.ajax({
-		url: "/<controller name>/check_id", 
+		url: "/id_check/check_id", 
 		type: "POST", 
 		data:{ 
 			"uid":uid
